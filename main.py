e28ed0503ff4a25447581d8e679a3cc883d7c38a
snowflake024 <smartcrap024@gmail.com>
2023-01-18 07:15:08 +0200

    finish first part

--- main.py
+++ main.py
@@ -1,43 +1,46 @@
 import datetime
 import win32com.client as win32
 
+folder = str(input("Choose a folder: "))
 today_date = str(datetime.date.today())
+
+# Variables related to win32com.client
 outlook = win32.Dispatch('outlook.application').GetNamespace("MAPI")
 inbox = outlook.GetDefaultFolder(6)
-message = inbox.Folders.Item("KPI")
-kpi_messages = message.Items
-
-if len(kpi_messages) == 0:
-    print("There aren't any messages in this folder")
-    exit()
-
-emails = []
-
-print(f"Today date is {today_date}")
-date_input = input("Please insect a date (follow above format): ")
-
-this_message = ()
-
-for message in kpi_messages:
-    # get some information about each message in a tuple
-    this_message = (
-        message.Subject,
-        message.SenderEmailAddress,
-        message.To,
-        message.Unread,
-        message.Senton.date(),
-        message.body,
-        message.Attachments
-    )
+message_object = inbox.Folders.Item(folder)
+items_messages = message_object.Items
 
 
-    emails.append(this_message)
-    split_from_address = message.SenderEmailAddress.partition("-")[2]
+def print_all_emails():
+    """Print all emails in a given outlook folder"""
 
-    # Print relevant email entry by date
-    # Add option to see all the emails available
-    if str(message.Senton.date()) == date_input:
-        print("am I being checked?")
-        print(message.Subject, message.Senton.date(), split_from_address)
+    if len(items_messages) == 0:
+        print("There aren't any messages in this folder")
         exit()
 
+    all_emails = []
+    for message in items_messages:
+        # get some information about each message in a tuple
+        this_message = (
+            message.Subject,
+            message.SenderEmailAddress.partition("-")[2],
+            message.To,
+            message.Unread,
+            message.Senton.date(),
+            message.body,
+            message.Attachments
+        )
+
+        all_emails.append(this_message)
+
+    print("List of all emails in the chosen folder: ")
+
+    for i in all_emails:
+        print(f"Subject: {i[0]}, Sender: {i[1]}, Sent on: {i[4]}")
+
+
+print("")
+print(f"Today's date is {today_date}")
+print("")
+print_all_emails()
+# choice = input("Please choose an email to be imported in the KPI Excel file")
